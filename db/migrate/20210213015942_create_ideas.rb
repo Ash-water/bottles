89641c3f8ec0d9f75c9a71dbab53ae0a36dff9c4
Ash-water <mizuwater78@gmail.com>
2021-02-13 11:02:15 +0900

    ideaモデルとテーブル作成

---
+++ db/migrate/20210213015942_create_ideas.rb
@@ -0,0 +1,9 @@
+class CreateIdeas < ActiveRecord::Migration[6.0]
+  def change
+    create_table :ideas do |t|
+      t.string :name, null: false
+      t.text :description, null: false 
+      t.timestamps
+    end
+  end
+end
