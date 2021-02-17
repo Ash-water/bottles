6134987b5ef75e059b09d3772d9e7f0ddcdbe619
Ash-water <mizuwater78@gmail.com>
2021-02-17 16:56:09 +0900

    全アクションルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "ideas#top"
   
-  resources :ideas, only: [:index, :new, :create]
+  resources :ideas
   get 'ideas/top', to: 'ideas#top'
 
 end
