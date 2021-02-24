7febf5acdafd469d23c9a90d319bb8e52caee619
Ash-water <mizuwater78@gmail.com>
2021-02-24 15:11:01 +0900

    バリデーションとアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :ideas
+
+  validates :nickname, presence: true
+  validates :email, presencee: true 
 end
